ff5383a5142b4504f441836e42fa1f68322a099f
Belendor <dwarf11@gmail.com>
2023-01-17 12:26:13 +0200

    update

--- terraform/init-script.sh
+++ terraform/init-script.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-sudo yum update -y
-sudo yum install httpd -y
-sudo systemctl enable httpd
-sudo systemctl start httpd
-echo "${file_content}!" > /var/www/html/index.html
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg lsb-release -y
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo sed -i 's/debian/ubuntu/g' /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
--- terraform/main.tf
+++ terraform/main.tf
@@ -8,38 +8,42 @@ data "aws_availability_zones" "available" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "2.64.0"
-
+  version = "3.18.1"
   name = "main-vpc"
-  cidr = var.vpc_cidr_block
+  cidr = "10.0.0.0/16"
 
   azs             = data.aws_availability_zones.available.names
-  private_subnets = slice(var.private_subnet_cidr_blocks, 0, var.private_subnet_count)
-  public_subnets  = slice(var.public_subnet_cidr_blocks, 0, var.public_subnet_count)
+  private_subnets = []
+  public_subnets  = ["10.0.101.0/24"]
 
   enable_nat_gateway = false
-  enable_vpn_gateway = var.enable_vpn_gateway
+  enable_vpn_gateway = false
 }
 
-module "app_security_group" {
-  source  = "terraform-aws-modules/security-group/aws//modules/web"
-  version = "3.17.0"
+resource "aws_security_group" "my-sg" {
+  vpc_id = module.vpc.vpc_id
+  name   = join("_", ["sg", module.vpc.vpc_id])
 
-  name        = "web-sg"
-  description = "Security group for web-servers with HTTP ports open within VPC"
-  vpc_id      = module.vpc.vpc_id
+  dynamic "ingress" {
+    for_each = var.rules
 
-  #   ingress_cidr_blocks = module.vpc.public_subnets_cidr_blocks
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-}
+    content {
+      from_port   = ingress.value["port"]
+      to_port     = ingress.value["port"]
+      protocol    = ingress.value["proto"]
+      cidr_blocks = ingress.value["cidr_blocks"]
+    }
+  }
 
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  tags = {
+    Name = "SG_Rules"
   }
 }
 
@@ -56,16 +60,16 @@ resource "aws_key_pair" "generated_key" {
 resource "aws_instance" "green" {
   count = 1
 
-  ami                    = data.aws_ami.amazon_linux.id
+  ami                    = "ami-0039da1f3917fa8e3"
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.generated_key.key_name
   subnet_id              = module.vpc.public_subnets[count.index % length(module.vpc.public_subnets)]
-  vpc_security_group_ids = [module.app_security_group.this_security_group_id]
+  vpc_security_group_ids = [aws_security_group.my-sg.id]
   user_data = templatefile("${path.module}/init-script.sh", {
     file_content = "Green - #${count.index}"
   })
 
   tags = {
-    Name = "green-${count.index}"
+    Name = "EC2.micro"
   }
 }
